59ae672a740866a3be7ee1255c5ba3b347d4e658
Unknown <lameesemad1996@hotmail.com>
2019-05-10 05:55:28 +0200

    periodic timer init finished, interrupt handler remains

---
+++ Project/Timer.h
@@ -0,0 +1,11 @@
+#ifndef __TIMER_H__
+#define	__TIMER_H__
+
+#include "typedefs.h"
+#include "tm4c123gh6pm.h"
+
+#define TIMER_LOAD_VAL 0x00F42400 // 16,000,000 in decimal = microcontroller's clock freq. therefore equivalent to one second
+
+void Periodic_Timer_Init(uint8 TimerNo);
+
+#endif
